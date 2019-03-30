350e9c4e0b702865ddcc0c7752ef45c632ff535a
David <aprendefacil1020@gmail.com>
2019-03-29 20:18:35 -0600

    Proyecto
    Integracion de Spring y JavaFX

---
+++ src/main/java/com/bytecode/javafx/spring/integration/SpringJavaFXIntegration/SpringJavaFxIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.bytecode.javafx.spring.integration.SpringJavaFXIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaFxIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaFxIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytecode/javafx/spring/integration/SpringJavaFXIntegration/SpringJavaFxIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bytecode.javafx.spring.integration.SpringJavaFXIntegration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringJavaFxIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
